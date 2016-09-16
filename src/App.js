bd01510a146f87a81c8c4315bd7b1cd163d05578
Robin Pokorný <me@robinpokorny.com>
2016-09-16 13:33:25 +0200

    Add Ratchet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { NavButton, NavBar, Title } from 'react-ratchet'
 
 export default ({ store }) => (
   <div className="App">
-    <h1>Citrus Home</h1>
-    <ul>
-      {Object.entries(store.devices).map(([key, value]) =>
-        <li key={key}>{value.label}</li>
-      )}
-    </ul>
+    <NavBar>
+      <NavButton href='#'>
+        Back
+      </NavButton>
+      <Title>Citrus Home</Title>
+    </NavBar>
+    <div className="content">
+
+      <ul>
+        {Object.entries(store.devices).map(([key, value]) =>
+          <li key={key}>{value.label}</li>
+        )}
+      </ul>
+      
+    </div>
   </div>
 )
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import entries from 'object.entries'
 entries.shim()
 
+import 'ratchet-npm/dist/css/ratchet.css'
+//import 'ratchet-npm/dist/css/ratchet-theme-ios.css'
+
 import ref from './firebase'
 import * as actions from './actions'
-import App from './App';
-import './index.css';
+import App from './App'
+import './index.css'
 
 ref.on('value', (snapshot) =>
   ReactDOM.render(
